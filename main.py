9e9b83000348be2ddebee74040da4b7619b99812
Hari815 <107530598+Hari815@users.noreply.github.com>
2022-08-03 10:23:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ async def start_handler(_, event: Message):
 
     await event.reply_text(Config.START_MSG.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("Our Channel", url="https://t.me/mtpmasala"),
-             InlineKeyboardButton("Our Group", url="https://t.me/mdisklinksbotchat")],
+            [InlineKeyboardButton("Our Channel", url="https://t.me/Telugu_Movies_999"),
+             InlineKeyboardButton("Our Group", url="https://t.me/movies_request_reply_group3")],
             [InlineKeyboardButton("Help", callback_data="Help_msg"),
              InlineKeyboardButton("About", callback_data="About_msg")]
         ])
@@ -40,8 +40,8 @@ async def help_handler(_, event: Message):
 
     await event.reply_text(Config.ABOUT_HELP_TEXT.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("Our Channel", url="https://t.me/mtpmasala"),
-             InlineKeyboardButton("Our Group", url="https://t.me/mdisklinksbotchat"), 
+            [InlineKeyboardButton("Our Channel", url="https://t.me/https://t.me/Telugu_Movies_999"),
+             InlineKeyboardButton("Our Group", url="https://t.me/https://t.me/movies_request_reply_group3"), 
              InlineKeyboardButton("About", callback_data="About_msg")]
         ])
     )
@@ -50,7 +50,7 @@ async def help_handler(_, event: Message):
 async def inline_handlers(_, event: Message):
     if event.text == '/start':
         return
-    answers = f'**📂 Results For ➠ {event.text} \n\n ➠ join us now @mtpmasala \n➠ Please Type Correct Spelling of movie and year for better experience .✍️\n➠ If any movie links not added or expired message here @mdisklinksbotchat.🗓️\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
+    answers = f'**📂 Results For ➠ {event.text} \n\n ➠ join us now @Telugu_Movies_999 \n➠ Please Type Correct Spelling of movie and year for better experience .✍️\n➠ If any movie links not added or expired message here @movies_request_reply_group3.🗓️\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
     async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=50, query=event.text):
         if message.text:
             thumb = None
@@ -81,8 +81,8 @@ async def button(bot, cmd: CallbackQuery):
 			reply_markup=InlineKeyboardMarkup(
 				[
 					[
-						InlineKeyboardButton("Our Channel", url="https://t.me/mtpmasala"),
-						InlineKeyboardButton("Our Group", url="https://t.me/mdisklinksbotchat")
+						InlineKeyboardButton("Our Channel", url="https://t.me/Telugu_Movies_999"),
+						InlineKeyboardButton("Our Group", url="https://t.me/movies_request_reply_group3")
 					],
 					[
 						InlineKeyboardButton("Developer", url="https://t.me/hari_813"),
@@ -100,10 +100,10 @@ async def button(bot, cmd: CallbackQuery):
 				[
 					[
 						InlineKeyboardButton("About", callback_data="About_msg"),
-						InlineKeyboardButton("Our Channel", url="https://t.me/mtpmasala")
+						InlineKeyboardButton("Our Channel", url="https://t.me/Telugu_Movies_999")
 					], 
                                         [
-						InlineKeyboardButton("Owner", url="https://t.me/hari_813"),
+						InlineKeyboardButton("Owner", url="https://t.me/arya_bro"),
 						InlineKeyboardButton("Home", callback_data="gohome")
 					]
 				]
@@ -121,8 +121,8 @@ async def button(bot, cmd: CallbackQuery):
 						InlineKeyboardButton("About", callback_data="About_msg")
 					],
 					[
-						InlineKeyboardButton("Support", url="https://t.me/mdisklinksbotchat"),
-						InlineKeyboardButton("Channel", url="https://t.me/mtpmasala")
+						InlineKeyboardButton("Support", url="https://t.me/movies_request_reply_group3")
+						InlineKeyboardButton("Channel", url="https://t.me/Telugu_Movies_999")
 					]
 				]
 			),
